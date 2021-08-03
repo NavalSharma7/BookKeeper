e95cc4e71e62d177a7d508dc7a6f87fca1b51267
NavalSharma7 <A00241484@myCambrian.ca>
2021-08-03 12:45:51 +0530

    Db context added, scafollding done for book category and category type.

---
+++ BookKeeper/Data/BookKeeperDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookKeeper.Data
+{
+    public class BookKeeperDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoryType> CategoryTypes { get; set; }
+
+
+        public BookKeeperDbContext(DbContextOptions<BookKeeperDbContext> options)
+           : base(options)
+        {
+
+        }
+    }
+}
